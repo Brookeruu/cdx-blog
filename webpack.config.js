7d804c867aa131de3b3ab3abf4ef7cbfa89745a7
Brooke <perrysbrooke@gmail.com>
2019-07-31 15:57:04 -0700

    edit Dotenv path

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,9 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new Dotenv()
+    new Dotenv({
+        path: path.resolve(__dirname, './.env'),
+    })
   ],
   devServer: {
     contentBase: './dist',
