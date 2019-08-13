da15b33c336598ba18ac059302e78bdd6cfe81ea
Brooke <perrysbrooke@gmail.com>
2019-08-13 12:23:27 -0700

    create context for admin login status

--- src/components/Firebase/FirebaseContext.jsx
+++ src/components/Firebase/FirebaseContext.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const FirebaseContext = React.createContext(null);
+const FirebaseContext = React.createContext({
+  admin: false,
+  setAdminLogIn: () => {},
+  setAdminLogOut: () => {},
+});
 
+export const FirebaseProvider = FirebaseContext.Provider;
+export const FirebaseConsumer = FirebaseContext.Consumer;
 export default FirebaseContext;
