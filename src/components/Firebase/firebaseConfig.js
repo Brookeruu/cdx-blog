e2afc43bd9919357c7d88c1c421401a59cd6af08
Brooke <perrysbrooke@gmail.com>
2019-08-20 16:15:48 -0700

    add fb database url

--- src/components/Firebase/firebaseConfig.js
+++ src/components/Firebase/firebaseConfig.js
@@ -5,7 +5,7 @@ import * as firebase from 'firebase';
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_APIKEY,
   authDomain: process.env.FIREBASE_AUTHDOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASEURL,
+  databaseURL: 'https://my-blog-677b8.firebaseio.com',
   projectId: process.env.FIREBASE_PROJECTID,
   storageBucket: process.env.FIREBASE_STORAGEBUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
